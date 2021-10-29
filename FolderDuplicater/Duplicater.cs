bcc3021c80c6b851d4c4120f4e24022223d2832f
Yotsura <gxb65358@gmail.com>
2021-10-29 17:24:31 +0900

    作業進捗バーを表示する機能追加

--- FolderDuplicater/Duplicater.cs
+++ FolderDuplicater/Duplicater.cs
@@ -28,20 +28,38 @@ namespace FolderDuplicater
                 return;
             }
 
-            var top = Console.CursorTop;
-            var left = Console.CursorLeft;
             Console.WriteLine($"\r\n更新対象のリストアップ中...");
-            _allfiles = Directory.EnumerateFiles(_origFolderPath, "*", System.IO.SearchOption.AllDirectories)
-                .AsParallel().Select(file => new FileData(pathinfo, file))
-                .Where(x => x.IsNewFile || x.IsUpdatedFile).ToList();
+            var checkList = Directory.EnumerateFiles(_origFolderPath, "*", System.IO.SearchOption.AllDirectories);
+            var checkListCnt = checkList.Count();
+
+            var cnt = 0;
+            int prePos = Console.CursorLeft;//現在カーソル位置を取得
+
+            _allfiles = checkList
+                .Select(file =>
+                {
+                    var temp = new FileData(pathinfo, file);
+
+                    var per = (double)cnt++ / (double)checkListCnt;
+                    Console.SetCursorPosition(0, Console.CursorTop);
+                    Console.Write(($"{GetProgressBar(20, per)}{cnt}/{checkListCnt}").PadRight(prePos));
+
+                    return temp;
+                }).Where(x => x.IsNewFile || x.IsUpdatedFile).ToList();
             //System.Threading.Thread.Sleep(3000);
 
-            Console.SetCursorPosition(left, top);
+            Console.WriteLine($"\r\n更新対象のリストアップ完了");
 
             var newfilecnt = _allfiles.Count(x => x.IsNewFile);
             var updatedfilecnt = _allfiles.Count(x => x.IsUpdatedFile);
-            Console.WriteLine($"\r\n更新対象は全{_allfiles.Count}件です。　　　　");
+            Console.WriteLine($"\r\n更新対象は全{_allfiles.Count}件です。");
             Console.WriteLine($"更新ファイル：{updatedfilecnt}件\r\n新規ファイル：{newfilecnt}件");
+        }
+
+        string GetProgressBar(int barLength, double per)
+        {
+            var bar = (int)Math.Floor(barLength * per);
+            return $"【{new string('■', bar)}{new string('□', barLength - bar)}】";
         }
 
         public void Dupticate()
