64f140ba2dd5e3af5d55f25b52f11f70264049e0
Yotsura <gxb65358@gmail.com>
2022-08-04 10:45:59 +0900

    barにタイトル追加。終了したのかもわかるように。

--- FolderDuplicater/ProgressBar.cs
+++ FolderDuplicater/ProgressBar.cs
@@ -4,6 +4,19 @@ namespace FolderDuplicater
 {
     public class ProgressBar
     {
+        public ProgressBar(double allCnt, int barLength, string barName)
+        {
+            _barLength = barLength;
+            _prePos = Console.CursorLeft;
+            _allCnt = allCnt;
+            Console.WriteLine($"＜{barName}＞");
+        }
+        public ProgressBar(double allCnt, string barName)
+        {
+            _prePos = Console.CursorLeft;
+            _allCnt = allCnt;
+            Console.WriteLine($"＜{barName}＞");
+        }
         public ProgressBar(double allCnt, int barLength)
         {
             _barLength = barLength;
@@ -20,13 +33,15 @@ namespace FolderDuplicater
         readonly int _prePos = 0;
         readonly double _allCnt = 0;
         double _cnt = 0;
+        bool IsCompleted => _cnt >= _allCnt;
 
         public void AddCnt()
         {
             _cnt++;
             double per = _cnt / _allCnt;
             Console.SetCursorPosition(0, Console.CursorTop);
-            Console.Write(($"{GetProgressBar(per)}{_cnt}/{_allCnt}").PadRight(_prePos));
+            Console.Write(($"{GetProgressBar(per)}{_cnt}/{_allCnt}{(per < 1 ? string.Empty : " COMPLETED\r\n")}")
+                .PadRight(_prePos));
         }
 
         private string GetProgressBar(double per)
