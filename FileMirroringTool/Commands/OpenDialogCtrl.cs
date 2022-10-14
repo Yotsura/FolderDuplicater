8309e830ba560cb2ac3dc0e56531e9db0ccb6c62
Yotsura <gxb65358@gmail.com>
2022-10-14 13:29:09 +0900

    フォルダ選択機能追加

---
+++ FileMirroringTool/Commands/OpenDialogCtrl.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.WindowsAPICodePack.Dialogs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace FileMirroringTool.Commands
+{
+    internal class OpenDialogCtrl : ICommand
+    {
+        readonly MainWindowViewModel _mwvm;
+        public OpenDialogCtrl(MainWindowViewModel mwvm)
+        {
+            _mwvm = mwvm;
+        }
+
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter) => true;
+
+        public void Execute(object parameter)
+        {
+            var path = FolderDialog();
+            if (string.IsNullOrEmpty(path)) return;
+            switch (parameter.ToString())
+            {
+                case "orig":
+                    _mwvm.OrigPath = path;
+                    break;
+                case "dest":
+                    _mwvm.DestPath += (string.IsNullOrEmpty(_mwvm.DestPath) ? string.Empty : "\n") + path;
+                    break;
+            }
+        }
+
+        public static string FolderDialog()
+        {
+            var dlg = new CommonOpenFileDialog("IFwwwフォルダ選択")
+            {
+                IsFolderPicker = true// フォルダ選択モード。
+            };
+            var ret = dlg.ShowDialog();
+            return ret == CommonFileDialogResult.Ok ? dlg.FileName : string.Empty;
+        }
+    }
+}
