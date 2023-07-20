38dc9f6c48b1fc2b422c14be7d716e93565f5e54
Yotsura <gxb65358@gmail.com>
2023-07-20 14:06:13 +0900

    stringExtension追加

---
+++ FileMirroringTool/Extensions/StringExtension.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileMirroringTool.Extensions
+{
+    internal static class StringExtension
+    {
+        internal static bool IsNullOrEmpty(this string orig)
+            => string.IsNullOrEmpty(orig);
+
+        internal static string GetRelativePath(this string fullPath, string basePath, string destPath)
+            => Path.Combine(destPath.TrimEnd(Path.DirectorySeparatorChar), fullPath.Substring(basePath.Length).TrimStart(Path.DirectorySeparatorChar));
+    }
+}
